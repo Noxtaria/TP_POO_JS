b5d6ccbdd9e8d70cd5d30921c66f30f951e80166
Kévin - Noxtaria <noxtariadev@gmail.com>
2023-01-29 16:38:19 +0100

    Correction du problème de multiplication

--- TP_POO_Contact/script.js
+++ TP_POO_Contact/script.js
@@ -2,38 +2,37 @@
 const btnSubmit = document.querySelector("#button");
 const tableData = document.querySelector("#data");
 
-
 let contacts = [
-    {
-        title : "Mr",
-        lastname : "WICK",
-        firstname : "John",
-        birthday : "21/12/1980",
-        phone : "+(33)6 45 12 74",
-        email : "jwick@gmail.com"
-    },
-    {
-        title : "Mr",
-        lastname : "PHIL",
-        firstname : "Spencer",
-        birthday : "14/04/1970",
-        phone : "+(33)6 12 01 39",
-        email : "philxbox@hotmail.com"
-    },
-    {
-        title : "Mme",
-        lastname : "LINE",
-        firstname : "Marie",
-        birthday : "10/06/1985",
-        phone : "+(33)7 24 71 12",
-        email : "mline@gmail.com"
-    }
-]
+  {
+    title: "Mr",
+    lastname: "WICK",
+    firstname: "John",
+    birthday: "21/12/1980",
+    phone: "+(33)6 45 12 74",
+    email: "jwick@gmail.com",
+  },
+  {
+    title: "Mr",
+    lastname: "PHIL",
+    firstname: "Spencer",
+    birthday: "14/04/1970",
+    phone: "+(33)6 12 01 39",
+    email: "philxbox@hotmail.com",
+  },
+  {
+    title: "Mme",
+    lastname: "LINE",
+    firstname: "Marie",
+    birthday: "10/06/1985",
+    phone: "+(33)7 24 71 12",
+    email: "mline@gmail.com",
+  },
+];
 // console.table(contacts);
 
-let addContact = () => {
-    contacts.forEach(contact => {
-        tableData.innerHTML += `<tr>
+const listContact = () => {
+  contacts.forEach((contact) => {
+    tableData.innerHTML += `<tr>
         <td>${contact.title}</td>
         <td>${contact.lastname}</td>
         <td>${contact.firstname}</td>
@@ -41,37 +40,32 @@ let addContact = () => {
         <td>${contact.phone}</td>
         <td>${contact.email}</td>
     </tr>
-    `
+    `;
     // console.table(contact)
-    })
-}
-
+  });
+};
 
 btnSubmit.addEventListener("click", (e) => {
-    e.preventDefault();
-    let contactMrTitle = document.querySelector("#title-mr").value;
-    // let contactMmeTitle = document.querySelector("#title-mme");
-    let contactLastname = document.querySelector("#lastname").value;
-    let contactFirstname = document.querySelector("#firstname").value;
-    let contactBirthday = document.querySelector("#birthday").value;
-    let contactPhone = document.querySelector("#phone").value;
-    let contactEmail = document.querySelector("#email").value;
+  e.preventDefault();
+  const newContact = {
+    title: document.querySelector("#title-mr").value,
+    // contactMmeTitle : document.querySelector("#title-mme"),
+    lastname: document.querySelector("#lastname").value,
+    firstname: document.querySelector("#firstname").value,
+    birthday: document.querySelector("#birthday").value,
+    phone: document.querySelector("#phone").value,
+    email: document.querySelector("#email").value,
+  };
 
-    contacts.push({
-        title : contactMrTitle,
-        // title : contactMmeTitle,
-        lastname : contactLastname,
-        firstname : contactFirstname,
-        birthday : contactBirthday,
-        phone : contactPhone,
-        email : contactEmail
-    })
+  tableData.innerHTML = "";
 
-    console.table(contacts);
-    addContact();
+  contacts.push(newContact);
 
-})
+  listContact();
+
+  console.table(contacts);
+});
 
 console.table(contacts);
 
-addContact();
+listContact();
