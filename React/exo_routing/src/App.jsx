e11d66937656f9ea911b9c36aa1e151b82eddecd
Kévin BUANNEC <noxtariadev@gmail.com>
2023-02-28 10:04:28 +0100

    Test

--- React/exo_routing/src/App.jsx
+++ React/exo_routing/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
           </button>
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav">
-              <NavLink className="nav-link" to="/">Home</NavLink>
+              <NavLink className="nav-link" to="/">Home</NavLink> 
               <NavLink className="nav-link" to="/project">My Projects</NavLink>
               <NavLink className="nav-link" to="/about">About</NavLink>
               <NavLink className="nav-link" to="/contact">Contact Me</NavLink>
